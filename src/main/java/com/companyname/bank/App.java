e0ae506604b03194d5d1eee017afce932bb7085f
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2020-04-27 19:04:24 +0530

    new modf

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "good evening..modify" );
+        System.out.println( "hello from new modf.." );
     }
 }
