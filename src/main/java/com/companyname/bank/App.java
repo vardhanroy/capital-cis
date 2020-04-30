9ec58a9c7717abe593691b7ff7dc55f31ca14ca6
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2020-04-30 18:29:48 +0530

    modf from github

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hello from new modf.." );
+        System.out.println( "hello from new modf..good evng" );
     }
 }
