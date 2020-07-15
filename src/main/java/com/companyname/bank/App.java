d75acebe325ac58cc57c7a25eb98e323294af40d
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2020-07-15 07:59:14 +0530

    new change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hello from proj..modf" );
+        System.out.println( "hello from proj..new change" );
     }
 }
