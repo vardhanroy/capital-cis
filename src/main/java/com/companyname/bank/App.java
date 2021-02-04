79b887a4332a0f810e81bde3e550df49e4a4464e
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-02-04 11:28:19 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hello from proj..hello..githubhhook" );
+        System.out.println( "hello new modf" );
     }
 }
