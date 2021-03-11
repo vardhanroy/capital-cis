7ef29067a8acb30fc0121952c518aa54fd21b416
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-03-11 07:51:32 +0530

    update

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hello new modf..modf from repo" );
+        System.out.println( "hello new modf..modf" );
     }
 }
