5ef2b1a403f0f7220a0f30a066687829d31fe04b
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-04-10 10:27:30 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello
 {
   public static void main()
   {
-    system.out.println("hello from ..new change from gith..modify")
+    system.out.println("hello from ..new change from gh..modify")
   }
 }
