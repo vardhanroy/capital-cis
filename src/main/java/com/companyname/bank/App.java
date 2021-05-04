94d545ce81745e113cc54d1f7bec1d4bf0e82851
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-05-04 18:24:38 +0530

    update src

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hello new modf..hello on github" );
+        System.out.println( "new change" );
     }
 }
