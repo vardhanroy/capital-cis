08cf61768f6a77fc4c6265480274b61d1f713ec2
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-05-31 07:47:03 +0530

    new change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hello..another change..NEW" );
+        System.out.println( "hello..one more change" );
     }
 }
