f55615e69ff073ce03ba44a6cc1f3f5e602f56dd
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-08-02 11:13:10 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello
 {
   public static void main()
   {
-    system.out.println("new chnage this change..something caged..n")
+    system.out.println("new chnage this change..something can")
   }
 }
