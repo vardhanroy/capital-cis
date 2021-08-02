7968cd7921af0300569cbc50c3c99ca64765ebc0
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-08-02 11:19:12 +0530

    asd

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello
 {
   public static void main()
   {
-    system.out.println("new chnage this change..something can")
+    system.out.println("new chnage this change..someng can")
   }
 }
