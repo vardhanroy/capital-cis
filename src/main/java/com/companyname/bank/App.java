68e51f66480d8d8e4bb7d0db08101abdae8c5ab5
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-09-21 11:44:53 +0530

    test commit

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new commit on github" );
+        System.out.println( "test commit" );
     }
 }
