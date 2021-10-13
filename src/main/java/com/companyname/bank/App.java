7507911a7383f3924a8c56d8d7bb8301713f7883
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-10-13 06:26:33 +0530

    new change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "change for test.. ");
+        System.out.println( "new change for gitscm poll ");
     }
 }
