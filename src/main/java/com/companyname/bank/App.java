fcb2e77520ba98beb5e3b37d834636a645dc00f3
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-11-03 09:54:02 +0530

    another modf

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new code change ");
+        System.out.println( "new code change..modify ");
     }
 }
