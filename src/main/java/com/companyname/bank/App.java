42203b987e51368f0c0a3cef9c8bbaad9b5d11b4
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-11-03 09:57:40 +0530

    new commit

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new code change..modify..new ");
+        System.out.println( "new code change..another new commit ");
     }
 }
