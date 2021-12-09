a2345de4daa12e7b6458e3325bf4a267e03d0b8a
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-12-09 10:38:27 +0530

    poll scm change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new code change..new change ");
+        System.out.println( "poll scm change ");
     }
 }
