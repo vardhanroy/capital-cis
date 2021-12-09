c20215be6f2ba8b474706ce3dad86332425f31c9
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-12-09 10:56:44 +0530

    github webhook change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "poll scm change ");
+        System.out.println( "github webhook change ");
     }
 }
