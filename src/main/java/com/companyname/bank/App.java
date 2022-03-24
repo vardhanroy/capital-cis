cff4552b5fa984c791f2bfcf4247c8f987ea457c
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-03-24 10:26:08 +0530

    webhook test

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "modify for pollscm.. ");
+        System.out.println( "change on repo for test webhook ");
     }
 }
