62a3f17e1f7ea5d0ddf3af80795bcf6362702904
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-03-24 10:00:09 +0530

    modify for pollscm

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github chnage on repo..new change.. ");
+        System.out.println( "modify for pollscm.. ");
     }
 }
