8ee669b119d58f0a0e0417ae02ad3f8999428b40
dev1 <dev1@mail.com>
2022-04-20 07:52:41 +0530

    new commit by dev1

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "another chnage ");
+        System.out.println( "another chnage..new commie by dev1 ");
     }
 }
