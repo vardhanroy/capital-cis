f6573323465e9628526401a9129da6bda68925ae
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-07-28 11:17:23 +0530

    new change on repo

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github hook change.. ");
+        System.out.println( "new change on the repo.. ");
     }
 }
