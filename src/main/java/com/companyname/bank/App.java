364f36c5b9592667ab36e3c5f842e3dcda6c16a5
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-11-02 11:00:36 +0530

    webhook change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new change on repo..poll scm ");
+        System.out.println( "new change on repo..from webhook ");
     }
 }
