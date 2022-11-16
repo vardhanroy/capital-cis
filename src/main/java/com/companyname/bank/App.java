64bf15adcf73349143db3b4c4bcb401be9126573
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-11-16 06:54:01 +0530

    poll scm change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "test change on github.. ");
+        System.out.println( "poll scm change ");
     }
 }
