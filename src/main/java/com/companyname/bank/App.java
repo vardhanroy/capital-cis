f3badefe0828acaaf1f0eb413c46a06381c0513f
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-12-06 12:08:12 +0530

    github new change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github webhook test ");
+        System.out.println( "github new change ");
     }
 }
