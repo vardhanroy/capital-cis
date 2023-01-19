057befe45deafee28f9500420bb8ae34eb03fd25
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-01-19 11:27:18 +0530

    new code changes

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new webhook change ");
+        System.out.println( "new code changes ");
     }
 }
