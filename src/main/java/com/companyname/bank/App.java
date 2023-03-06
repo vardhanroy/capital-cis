89e4cc2a32cf2fe3e590ad3a79ac97417e7a7589
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-03-06 12:31:34 +0530

    new project change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "testing webhook.. ");
+        System.out.println( "new chnage of project ");
     }
 }
