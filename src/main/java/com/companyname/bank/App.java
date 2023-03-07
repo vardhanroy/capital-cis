916c9fe840c666df75e6c00d694f56081fe40f3b
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-03-07 12:23:25 +0530

    poll scm change on github

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new chnage of project ");
+        System.out.println( "poll scm change on github ");
     }
 }
