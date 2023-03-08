68806a12f295119c8aae582616250c0622da21c6
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-03-08 11:54:32 +0530

    New github event

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github webhook event test  ");
+        System.out.println( "New github webhook event test  ");
     }
 }
