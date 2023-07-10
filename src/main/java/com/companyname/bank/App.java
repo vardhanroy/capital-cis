42b474dc217f38b76b58680a8523b6760bde92bf
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-07-10 10:54:50 +0530

    change on github

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "New github hook test commit ");
+        System.out.println( "change on github ");
     }
 }
