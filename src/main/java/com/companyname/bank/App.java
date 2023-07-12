9ab33913240c79cefd116ecad3268441909aa3e1
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-07-12 10:59:43 +0530

    poll scm test change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "change on github ");
+        System.out.println( "poll scm test change ");
     }
 }
