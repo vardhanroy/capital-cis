96f73619ec52a2a904267c7b14fa041c62f67bd2
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-10-12 08:55:03 +0530

    test change for poll scm on repo

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new code change on the project");
+        System.out.println( "change to test poll scm");
     }
 }
