9c983246bb18fdcabd10c9650f75726e72863263
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-10-13 08:22:58 +0530

    github test commit

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "change to test poll scm");
+        System.out.println( "github test commit");
     }
 }
