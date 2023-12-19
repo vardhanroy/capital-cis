0bc8adf42d2f480563593e3c6b9051ecc2e58798
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-12-19 10:15:31 +0530

    code changes

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github hook trigger");
+        System.out.println( "modify project code");
     }
 }
