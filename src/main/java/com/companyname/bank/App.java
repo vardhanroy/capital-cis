67bf49eaf828cb62f9d5cb4686a52159bd545a69
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-12-21 09:54:11 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "modify project code");
+        System.out.println( "new code changes");
     }
 }
