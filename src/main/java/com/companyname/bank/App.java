87fc6bf772b577a46da725a26b663630adb58e09
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2024-05-09 09:36:46 +0530

    new repo changes

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "code changes on repo..");
+        System.out.println( "new changes on the repo..");
     }
 }
