cc879cc2c519c8566908bd1c29b6d772992e8d9f
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2024-09-02 09:54:04 +0530

    update code

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new code changes on github..");
+        System.out.println( "update the code..");
     }
 }
