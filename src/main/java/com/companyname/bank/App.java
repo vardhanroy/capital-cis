fb3b429ff2161f6be59284ebbeaa2897c96447bc
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2024-09-03 10:08:57 +0530

    poll scm change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "update the code..");
+        System.out.println( "new commit for pollscm");
     }
 }
