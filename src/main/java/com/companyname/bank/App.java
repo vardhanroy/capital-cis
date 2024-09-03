426f6f99f6cc51f332bdb1bf869efb356af1dae4
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2024-09-03 10:23:10 +0530

    github test commit for webhook

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new commit for pollscm");
+        System.out.println( "new commit for github hook");
     }
 }
