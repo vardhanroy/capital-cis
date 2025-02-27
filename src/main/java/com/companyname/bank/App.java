d7712cf54631bcd0859812266511b71af2a6c584
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2025-02-27 08:29:14 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github hook test commit");
+        System.out.println( "code update in github");
     }
 }
