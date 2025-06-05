bdef4e08d384fa1c3ebcc73c3132a220a49d4dca
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2025-06-05 07:23:40 +0530

    test commit for pollscm

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github source code update");
+        System.out.println( "TEST commit for pollscm");
     }
 }
