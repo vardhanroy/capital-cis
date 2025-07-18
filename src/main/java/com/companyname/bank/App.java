aeddc38c347fff8baa53db83d016e77cc514ed33
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2025-07-18 09:40:22 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new commit for github webhook");
+        System.out.println( "code change by dev");
     }
 }
